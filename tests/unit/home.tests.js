91e10985c5db4144a0095442c18c37cf4d32f002
Yordan Valkov <yordan.valkov@gmail.com>
2024-06-24 21:49:25 +0300

    update homepage and tests

--- tests/unit/home.tests.js
+++ tests/unit/home.tests.js
@@ -5,7 +5,7 @@ suite('Home page', function() {
   test('Page title', async function() {
     let res = await fetch("http://localhost:8080/");
     let body = await res.text();
-    assert.ok(body.includes("<h1>Cookbook DevOps</h1>"));
+    assert.ok(body.includes("<h1>Cookbook</h1>"));
   });
   
   test('Products count', async function() {
