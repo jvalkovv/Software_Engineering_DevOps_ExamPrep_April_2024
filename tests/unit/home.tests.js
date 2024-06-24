deaded4c99e8124ef3dd4eb4507eb262585446bb
Yordan Valkov <yordan.valkov@gmail.com>
2024-06-24 21:39:39 +0300

    Edit files

--- tests/unit/home.tests.js
+++ tests/unit/home.tests.js
@@ -5,12 +5,12 @@ suite('Home page', function() {
   test('Page title', async function() {
     let res = await fetch("http://localhost:8080/");
     let body = await res.text();
-    assert.ok(body.includes("<h1>Cookbook</h1>"));
+    assert.ok(body.includes("<h1>Cookbook DevOps</h1>"));
   });
   
   test('Products count', async function() {
     let res = await fetch("http://localhost:8080/");
     let body = await res.text();
-    assert.ok(body.includes("Cookbook DevOps: <b>3</b>"));
+    assert.ok(body.includes("Cookbook: <b>3</b>"));
   });
 });
